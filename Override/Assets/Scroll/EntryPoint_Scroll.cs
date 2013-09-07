497b304b0ed9167f1d2275f873cd58933b167482
Toru <toru.inoue@kissaki.tv>
2013-09-07 10:52:22 +0900

    update README

--- Override/Assets/Scroll/EntryPoint_Scroll.cs
+++ Override/Assets/Scroll/EntryPoint_Scroll.cs
@@ -7,8 +7,14 @@ public class EntryPoint_Scroll : MonoBehaviour {
 		ステージ進行、入力に対するゲーム側の挙動を行う。
 
 		イェーガーのなかの人からのシグナルを受けて、イェーガーの操作、KJの操作、判定を行う。
+		・基礎的な進行をAppControllerが制御(stateを持つ)
+		・ゲーム進行はこのScrollが持つ
+		という原則で動かす。
 	*/
-	void Awake () {
 
+	public GameObject scroll;
+
+	void Awake () {
+		// scroll = GameObject.
 	}
 }
